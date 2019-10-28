7e408cb4ac2977499ad95d6fd829b9207dd02ea2
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2019-10-28 16:22:12 -0300

    Aula 3 - liberação de endpoints e autenticação de usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$N8a7Xd1ITA6VgciWZofjr.VR.G..VrQSmRjFpk5jS94kl9nPuAXXy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
